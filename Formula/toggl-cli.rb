44b9fb05950d80f158af0129e063ff26f6a8b9ab
Sanyu Melwani <1154057+sanyu@users.noreply.github.com>
2024-11-05 10:00:00 +0000

    Upgrade toggl-cli to v0.4.7

--- Formula/toggl-cli.rb
+++ Formula/toggl-cli.rb
@@ -1,17 +1,17 @@
 class TogglCli < Formula
   desc "Command line app for Toggl Track time tracking"
   homepage "https://github.com/watercooler-labs/toggl-cli"
-  version "0.4.5"
+  version "0.4.7"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/watercooler-labs/toggl-cli/releases/download/v0.4.5/toggl-aarch64-apple-darwin-0.4.5.zip"
-      sha256 "7a2b949e41910cb59e8fd29fb5122608b17e7c9c0965d9e3f2e8c0017f4276a9"
+      url "https://github.com/watercooler-labs/toggl-cli/releases/download/v0.4.7/toggl-aarch64-apple-darwin-0.4.7.zip"
+      sha256 "513361a5ab1b3476275252530227c290878d7c7cdebd2e78b8d0d3e5dcdd77cb"
     end
     on_intel do
-      url "https://github.com/watercooler-labs/toggl-cli/releases/download/v0.4.5/toggl-x86_64-apple-darwin-0.4.5.zip"
-      sha256 "9f79297132cecc15d41a4062e9e0f3aa99693da921c3d133c7f47412c2b15743"
+      url "https://github.com/watercooler-labs/toggl-cli/releases/download/v0.4.7/toggl-x86_64-apple-darwin-0.4.7.zip"
+      sha256 "079f69ab6ebccd688f00f5532c288704d7f69417459983876122df6b916cf54e"
     end
   end
 
