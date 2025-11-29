8fe5c06e57e1bf216d101085a25633ff55b43063
Sanyu Melwani <1154057+sanyu@users.noreply.github.com>
2025-11-29 13:30:00 +0000

    tg: update to 1.0.9
    - Fix Universal Action to use argv input

--- Formula/tg.rb
+++ Formula/tg.rb
@@ -1,8 +1,8 @@
 class Tg < Formula
   desc "Toggl CLI wrapper with clipboard support for macOS"
   homepage "https://github.com/sanyu/alfred-toggl"
-  url "https://github.com/sanyu/alfred-toggl/archive/refs/tags/v1.0.8.tar.gz"
-  sha256 "5ee26c249b73ec8fc0a413d7e318d4f75223f172465418321eb27c6b70c301e3"
+  url "https://github.com/sanyu/alfred-toggl/archive/refs/tags/v1.0.9.tar.gz"
+  sha256 "78c29a5b5de3a9f9e1548bf3971c02537f1e0018d0ba10d6295e8b42b907c90b"
   license "MIT"
 
   depends_on "sanyu/tap/toggl-cli"
